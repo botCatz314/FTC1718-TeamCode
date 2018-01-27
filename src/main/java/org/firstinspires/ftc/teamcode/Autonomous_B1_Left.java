8b1465a8958ccb44bdc5281af10aa92de6c8906c
LAPTOP-J26LMCR5\ITSA-5 <botcatz314@gmail.com>
2018-01-26 19:12:43 -0700

    Commented on the B1 Left code

--- src/main/java/org/firstinspires/ftc/teamcode/Autonomous_B1_Left.java
+++ src/main/java/org/firstinspires/ftc/teamcode/Autonomous_B1_Left.java
@@ -38,22 +38,19 @@ public class Autonomous_B1_Left extends LinearOpMode {
     private ColorSensor colorSensor;     //declares the color sensor
     private Servo servoStick; //declares servos
     private Servo servoStick2;
+
     double pi = 3.1415926535;
     double powerOff = 0; //creates a variable equal to zero so that the motors can turn off without the use of a magic number
     BNO055IMU imu; //declares integrated gyro
     private boolean NEEDSTOBEDELETED = true;
-    public boolean Right = false;
-    public boolean Center = false;
-    public boolean Left = false;
+    public boolean Right = false; //Variable for the Vuforia code
+    public boolean Center = false;//Variable for the Vuforia code
+    public boolean Left = false;  //Variable for the Vuforia code
     public static final String TAG = "Vuforia VuMark Sample";
     public boolean UsingEncoders = false;
 
     OpenGLMatrix lastLocation = null;
 
-    /**
-     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
-     * localization engine.
-     */
     VuforiaLocalizer vuforia;
 
     @Override
@@ -69,36 +66,19 @@ public class Autonomous_B1_Left extends LinearOpMode {
         servoStick2 = hardwareMap.servo.get("servoStick2");
         rightMotor.setDirection(DcMotor.Direction.REVERSE);//sets the right motors reverse
         rightMotor2.setDirection(DcMotor.Direction.REVERSE); //sets the right motors reverse
-                /*
-         * To start up Vuforia, tell it the view that we wish to use for camera monitor (on the RC phone);
-         * If no camera monitor is desired, use the parameterless constructor instead (commented out below).
-         */
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        VuforiaLocalizer.Parameters parametersV = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
 
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName()); //Uses camera viewer on the phone
+        VuforiaLocalizer.Parameters parametersV = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
         // OR...  Do Not Activate the Camera Monitor View, to save power
         // VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-
-
         parametersV.vuforiaLicenseKey = "AeOpxj3/////AAAAGa1hky4Ahkp6jA7uCGunP+KJAZb3Di06YSh1ToEAxDmlWGeqxY3Mp26DqFw1P5Lyc/gFq992XUJ2bf8QtwYWln76jzRISvwAoSotdCOMreIL6fpbK4fdsAG9u85FTJlPDsOMY5u9YktxQ/JERWyrQC/NhAxJX+RDVtTouFnrUx/EI8CJDHR/IFcHnQ4KIJdCfQBoeC6+qMJ1RCa2lo2BFPcQv4blFatYz4Z0P+0XVhiza0t0mwJXKzTlwq+c4V9X0nWseTQZXnmgbB0kwQx+m/pGzr9ImML9WhSiWp5qPjyqDYitWs7cU/zWLFFT1wWpW7KkhQ+boQ2zwUsYKemRKY21LV9lkHh5/2a7bJWqKHY/";
-
-        /*
-         * We also indicate which camera on the RC that we wish to use.
-         * Here we chose the back (HiRes) camera (for greater range), but
-         * for a competition robot, the front camera might be more convenient.
-         */
-        parametersV.cameraDirection = VuforiaLocalizer.CameraDirection.FRONT;
+        parametersV.cameraDirection = VuforiaLocalizer.CameraDirection.FRONT; //Decides which camera to use
         this.vuforia = ClassFactory.createVuforiaLocalizer(parametersV);
-
-        /**
-         * Load the data set containing the VuMarks for Relic Recovery. There's only one trackable
-         * in this data set: all three of the VuMarks in the game were created from this one template,
-         * but differ in their instance id information.
-         * @see VuMarkInstanceId
-         */
-        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
-        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark"); //Decides which pictures to use
+        VuforiaTrackable relicTemplate = relicTrackables.get(0); //Base pictures
         relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+
 
         //sets parameters of gyro
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
@@ -109,52 +89,41 @@ public class Autonomous_B1_Left extends LinearOpMode {
         parameters.loggingTag = "IMU";
         parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
         imu.initialize(parameters);
-
-
         imu.readCalibrationData(); //calibrates gyro
         imu.isGyroCalibrated(); //checks that gyro is calibrated
         //shows user that gyro is calibrated
 
-        relicTrackables.activate();
+
+        relicTrackables.activate(); //Activates Vuforia
         waitForStart(); //waits until the user presses play
         while (opModeIsActive()) {
 
-            /**
-             * See if any of the instances of {@link relicTemplate} are currently visible.
-             * {@link RelicRecoveryVuMark} is an enum which can have the following values:
-             * UNKNOWN, LEFT, CENTER, and RIGHT. When a VuMark is visible, something other than
-             * UNKNOWN will be returned by {@link RelicRecoveryVuMark#from(VuforiaTrackable)}.
-             */
+            //Sets variable for figuring out which picture it is
             RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+            //Checks if the VuMark is unknown and acts based on that
             if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
 
-                /* Found an instance of the template. In the actual game, you will probably
-                 * loop until this condition occurs, then move on to act accordingly depending
-                 * on which VuMark was visible. */
                 telemetry.addData("VuMark", "%s visible", vuMark);
 
-                /* For fun, we also exhibit the navigational pose. In the Relic Recovery game,
-                 * it is perhaps unlikely that you will actually need to act on this pose information, but
-                 * we illustrate it nevertheless, for completeness. */
-                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) relicTemplate.getListener()).getPose();
+                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) relicTemplate.getListener()).getPose(); //Gets the angle of the picture
 
                 /* We further illustrate how to decompose the pose into useful rotational and
                  * translational components */
                 if (pose != null) {
                     VectorF trans = pose.getTranslation();
                     Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
-
                     // Extract the X, Y, and Z components of the offset of the target relative to the robot
                     double tX = trans.get(0);
                     double tY = trans.get(1);
                     double tZ = trans.get(2);
-
                     // Extract the rotational components of the target relative to the robot
                     double rX = rot.firstAngle;
                     double rY = rot.secondAngle;
                     double rZ = rot.thirdAngle;
                 }
             }
+
+            //Runs a check to see which of the picture is active
             if (vuMark == RelicRecoveryVuMark.CENTER) {
                 Center = true;
                 telemetry.addData("Bool", "Center");
@@ -178,13 +147,11 @@ public class Autonomous_B1_Left extends LinearOpMode {
             } else {
                 telemetry.addData("VuMark", "not visible");
             }
-
             telemetry.update();
         }
 
         servoStick2.setPosition(1);//servo stick down motion
         sleep(3000);//pause code for 2 seconds
-        //
 
         try {
             if (colorSensor.red() * 1.1 > colorSensor.blue()) {// asking if red's presence is greater than blue
@@ -192,22 +159,34 @@ public class Autonomous_B1_Left extends LinearOpMode {
                 telemetry.addData("blue: ", colorSensor.blue());//prints blue's value to phone
                 telemetry.update();//prints the telemetry to screen
                 sleep(4000); //pause for 4 seconds
+
                 //    Drive_Forwards(.2);// moves forwards at 40% power
+
                 sleep(500); // pause for 1/2 second
+
                 //    Brake(); // stops wheel motion
+
                 sleep(500);
                 servoStick2.setPosition(0); // bring up the jewel stick
+
                 //   Drive_Backwards(.2);//drives to roughly the same place as other part of if statement
+
                 sleep(1000);//pauses for one second
+
                 //    Brake();//stops all wheel movement
+
             } else if (colorSensor.blue() * 1.1 > colorSensor.red()) {// if blue is greater than red...
                 telemetry.addData("red: ", colorSensor.red());// queues value of red to be printed
                 telemetry.addData("blue: ", colorSensor.blue());//queues value of blue to be printed
                 telemetry.update();// prints queued data to phone
                 sleep(4000); // stops for 4 seconds
+
                 //    Drive_Backwards(.2); // drives backwards at 40% power
+
                 sleep(500); // stops for 1/2 a seconds
+
                 //   Brake();// stops wheel movement
+
                 servoStick2.setPosition(0);// bring up jewel whacker
             } else {
                 servoStick2.setPosition(0);
@@ -219,91 +198,41 @@ public class Autonomous_B1_Left extends LinearOpMode {
         }
         servoStick2.setPosition(0);// auxiliary bringing up of jewel whacker
        // DriveWithEncoders(20, .9);
-    }
 
-     /*   if (Left = true && UsingEncoders = false) {
 
-            Drive_Backwards(.2);
-            sleep(600);
-            Brake();
-
-            Drive_Backwards(.3);
-            sleep(2300);
-            Brake();
-
-            Turn_Right(.4);
-            sleep(600);
-            Brake();
-
-            telemetry.update();
-            sleep(15000);
-        }
-        if (Center = true && UsingEncoders = false) {
-
-            Drive_Backwards(.2);
-            sleep(600);
-            Brake();
-
-            Turn_Right(.4);
-            sleep(600);
-            Brake();
-
-            Drive_Backwards(.3);
-            sleep(2300);
-            Brake();
-
-            telemetry.update();
-            sleep(15000);
-        }
-       // if (Right = true && UsingEncoders = false) {
-
-            Drive_Backwards(.2);
-            sleep(600);
-            Brake();
-
-            Turn_Right(.6);
-            sleep(600);
-            Brake();
-
-            Drive_Backwards(.3);
-            sleep(2300);
-            Brake();
-
-            telemetry.update();
-            sleep(15000);
-        }
-
-    }
     private void Drive_Forwards(double power){//function for driving forwards
         leftMotor.setPower(power);//this turns on the left motor=to power input
         rightMotor.setPower(power);//this turns on the right motor=to power input
         rightMotor2.setPower(power);// this turns on the right motor2=to power input
         leftMotor2.setPower(power);//this turns on the left motor2=to power input
     }
+
     private void Brake(){//stops all wheel movement
         leftMotor.setPower(0);//brakes left motor
         rightMotor.setPower(0.0);//brakes right motor
         rightMotor2.setPower(0.0);//brakes right motor2
         leftMotor2.setPower(0.0);//brakes left motor2
     }
+
     private void Drive_Backwards(double power){//reverse all wheels
         leftMotor.setPower(-power);//left motor drives in reverse
         rightMotor.setPower(-power);//right motor drives in reverse
         rightMotor2.setPower(-power);//right motor2 drives in reverse
         leftMotor2.setPower(-power);//left motor2 drives in reverse
     }
+
     private void Turn_Right(double power){//drives motors to turn right
         leftMotor.setPower(power);//drives left motor forwards
         rightMotor.setPower(-power);//drives left motor backwards
         rightMotor2.setPower(-power);//drives right motor2 backwards
         leftMotor2.setPower(power);//drives left motor2 forwards
     }
+
     private void Turn_Left (double power){//drives all motors left
         leftMotor.setPower(-power);//drives left motor backwards
         rightMotor.setPower(power);//drives right motor forwards
         rightMotor2.setPower(power);//drives right motor2 forwards
         leftMotor2.setPower(-power);//drives left motor2 backwards
-    */
 
         public void DriveWithEncoders(double distance, double speed) {
             double off = 0;
@@ -342,10 +271,7 @@ public class Autonomous_B1_Left extends LinearOpMode {
                 leftMotor2.setPower(off);
                 rightMotor2.setPower(off);
 
-
             }
         }
-
-
     }
 
