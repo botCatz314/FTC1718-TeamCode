093df0449485b5cce3bbe24965093c3dc28fe810
botCatz <botcatz314@gmail.com>
2018-02-06 20:24:23 -0700

    dpad for linear slide

--- src/main/java/org/firstinspires/ftc/teamcode/botCatzTeleOp_SingleStick.java
+++ src/main/java/org/firstinspires/ftc/teamcode/botCatzTeleOp_SingleStick.java
@@ -21,6 +21,7 @@ public class botCatzTeleOp_SingleStick extends LinearOpMode {
     private float x, y, z, w, pwr;
     private static double deadzone = 0.2;
     private static boolean TankDriveActive = false, clawHold = false;
+    private static boolean lsExtendPos = false, lsExtendNeg = false, lsRetractPos = false, lsRetractNeg = false;
 
     // declare motors as variables
     private DcMotor leftMotor, rightMotor; //declares drive motors
@@ -30,7 +31,7 @@ public class botCatzTeleOp_SingleStick extends LinearOpMode {
     private Servo servoStickRight1,servoStickLeft2; //declares servos with regular range of motion
     private DcMotor clawMotor;
 
-    //private CRServo clawAngle; 
+    //private CRServo clawAngle;
     public void runOpMode() {
         //defines drive motors
         leftMotor = hardwareMap.dcMotor.get("leftMotor");
@@ -52,154 +53,121 @@ public class botCatzTeleOp_SingleStick extends LinearOpMode {
 
         waitForStart();//waits until we hit play
 
-        while (opModeIsActive()) {//it loops the code inside until we hit stop
+        while(opModeIsActive()) {//it loops the code inside until we hit stop
 
-            /*leftMotor.setPower(gamepad1.left_stick_y);//sets power of leftMotor = to joystick value
-            leftMotor2.setPower(gamepad1.left_stick_y);//sets power of leftMotor2 = to joystick value
-            rightMotor.setPower(gamepad1.right_stick_y);//sets power of rightMotor = to joystick value
-            rightMotor2.setPower(gamepad1.right_stick_y);//sets power of rightMotor2 = to joystick value
-            */
-
-            // Different joystick controls:
-            getJoyVals();
-            pwr = y; //this can be tweaked for exponential power increase
-            if (rightMotor != null) rightMotor.setPower(Range.clip(pwr - x + z, -1, 1));
-            leftMotor.setPower(Range.clip(pwr - x - z, -1, 1));
-            telemetry.addData("x: ", x);
-            telemetry.addData("y: ", y);
-            telemetry.addData("pwr: ", pwr);
-            //telemetry.update();
-            // end different controls
-
-            slideMotor.setPower(gamepad2.right_stick_y);//sets power of linearSlide = to joystick value
-            if (gamepad2.right_stick_y > 1.0) {
-                gamepad2.right_stick_y = 1.0f;
-                slideReverse.setPower(0.5);
-            } else if (gamepad2.right_stick_y < -0.5) {
-                gamepad2.right_stick_y = -0.5f;
-                slideReverse.setPower(-1.0);
-            }
-            clawMotor.setPower(gamepad2.left_stick_y);//sets power of clawMotor = to joystick value
-            armHeight.setPower(-gamepad2.right_trigger);//sets the motor controlling arm height equal to the negative right trigger
-            armHeight.setPower(gamepad2.left_trigger); //sets the motor controlling arm height equal to the left trigger
-            telemetry.addData("left stick: ", gamepad2.left_stick_y);
-            telemetry.addData("right stick: ", gamepad2.right_stick_y);
-            telemetry.addData("right trigger: ", gamepad2.right_trigger);
-            telemetry.addData("left trigger: ", gamepad2.left_trigger);
-            telemetry.update();
-           /* if(gamepad2.a){
-                clawAngle.setPower(1.0);
-            }
-            if(gamepad2.b){
-                clawAngle.setPower(-1.0);
-            }
-            if(gamepad2.x){
-                clawAngle.setPower(0.0);
-            }*/
-
-            if (gamepad1.a) {//if game pad 1 a is being pressed...
-                servoStickRight1.setPosition(0);//servo stick is set to position 0
-
-                if (TankDriveActive == true) {
+            if (TankDriveActive == true){
                 /*leftMotor.setPower(gamepad1.left_stick_y);//sets power of leftMotor2 = to joystick value
                 rightMotor.setPower(gamepad1.right_stick_y);//sets power of rightMotor2 = to joystick value
                 leftMotor.setPower(gamepad1.right_stick_x);
                 rightMotor.setPower(-gamepad1.right_stick_x);
                 */
-                    //leftMotor.setPower(-gamepad1.left_stick_y);
-                    leftMotor.setPower(gamepad1.left_stick_y);
-                    //rightMotor.setPower(-gamepad1.right_stick_y);
-                    rightMotor.setPower(gamepad1.right_stick_y);
-                } else {
-                    getJoyVals();
-                    pwr = y; //this can be tweaked for exponential power increase
-                    //sets power of drive motors
-                    rightMotor.setPower(Range.clip(pwr - x + z, -1, 1));
-                    leftMotor.setPower(Range.clip(pwr - x - z, -1, 1));
-                    //returns data to phones
-                    telemetry.addData("x: ", x);
-                    telemetry.addData("y: ", y);
-                    telemetry.addData("pwr: ", pwr);
-                    telemetry.update();
-                }
-                if (gamepad1.a) {
-                    TankDriveActive = true;
-                }
-                if (gamepad1.b) {
-                    TankDriveActive = false;
-                }
-
-                //controls the linearSlide
-
-                if (gamepad2.right_stick_y > 0.1) {
-                    slideReverse.setPower(0.5 * gamepad2.right_stick_y);
-                    slideMotor.setPower(gamepad2.right_stick_y);
-                }
-                if (gamepad2.right_stick_y < -0.1) {
-                    slideReverse.setPower(0.5 * gamepad2.right_stick_y);
-                    slideMotor.setPower(gamepad2.right_stick_y);
-                }
-
-                if (gamepad2.dpad_down) {
-                    slideMotor.setPower(-0.25);
-                }
-                if (gamepad2.dpad_up) {
-                    slideMotor.setPower(0.25);
-                }
-                if (gamepad2.dpad_left) {
-                    slideReverse.setPower(-0.25);
-                }
-                if (gamepad2.dpad_right) {
-                    slideReverse.setPower(0.25);
-                }
-
-                //sets power of clawMotor
-                clawMotor.setPower(-gamepad2.right_trigger); //Close claw
-                clawMotor.setPower(gamepad2.left_trigger); //Open claw
-
-                // clawMotor hold
-                if (gamepad2.right_bumper) {
-                    clawHold = true;
-                }
-                if (gamepad2.left_bumper) {
-                    clawHold = false;
-                }
-                if (clawHold) {
-                    // hold claw closed
-                    clawMotor.setPower(-0.4);
-                }
-
-                //controls the height of the linear slide
-                armHeight.setPower(-gamepad2.left_stick_y);//sets the motor controlling arm height equal to the left stick
-
-                //gives drivers control of the servoStick in case it does not pull up
-                if (gamepad2.a) {
-                    servoStickRight1.setPosition(1);
-                    telemetry.addData("a", "true");
-                    telemetry.update();
-                }
-                if (gamepad2.b) {
-                    servoStickRight1.setPosition(0);
-                    telemetry.addData("b", "true");
-                    telemetry.update();
-                }
-                if (gamepad2.x) {
-                    servoStickLeft2.setPosition(1);
-                    telemetry.addData("Correct", "true");
-                    telemetry.update();
-                }
-                if (gamepad2.y) {
-                    servoStickLeft2.setPosition(0);
-                    telemetry.addData("Wrong", "true");
-                    telemetry.update();
-                }
-
-                idle();//waits to be caught up
-
+                //leftMotor.setPower(-gamepad1.left_stick_y);
+                leftMotor.setPower(gamepad1.left_stick_y);
+                //rightMotor.setPower(-gamepad1.right_stick_y);
+                rightMotor.setPower(gamepad1.right_stick_y);
             }
+            else {
+                getJoyVals();
+                pwr = y; //this can be tweaked for exponential power increase
+                //sets power of drive motors
+                rightMotor.setPower(Range.clip(pwr - x+z, -1, 1));
+                leftMotor.setPower(Range.clip(pwr - x-z, -1, 1));
+                //returns data to phones
+                telemetry.addData("x: ", x);
+                telemetry.addData("y: ", y);
+                telemetry.addData("pwr: ", pwr);
+                telemetry.update();
+            }
+            if (gamepad1.a) {
+                TankDriveActive = true;
+            }
+            if (gamepad1.b) {
+                TankDriveActive = false;
+            }
+
+            //controls the linearSlide
+
+            if(gamepad2.right_stick_y > 0.1){
+                slideReverse.setPower(0.5*gamepad2.right_stick_y);
+                slideMotor.setPower(gamepad2.right_stick_y);
+            } else if(gamepad2.right_stick_y < -0.1){
+                slideReverse.setPower(0.5*gamepad2.right_stick_y);
+                slideMotor.setPower(gamepad2.right_stick_y);
+            } else {
+                slideReverse.setPower(0);
+                slideMotor.setPower(0);
+            }
+
+            if (gamepad2.dpad_down){
+                lsExtendNeg = true;
+                if (lsExtendNeg) lsExtendPos = false;
+            }
+            if (gamepad2.dpad_up){
+                lsExtendPos = true;
+                if (lsExtendPos) lsExtendNeg = false;
+            }
+            if (gamepad2.dpad_left){
+                lsRetractPos = true;
+                if (lsRetractPos) lsRetractNeg = false;
+            }
+            if (gamepad2.dpad_right){
+                lsRetractNeg = true;
+                if (lsRetractNeg) lsRetractPos = false;
+            }
+
+            if (lsExtendNeg) slideMotor.setPower(-0.25);
+
+            if (lsExtendPos) slideMotor.setPower(0.25);
+
+            if (lsRetractNeg) slideReverse.setPower(-0.25);
+
+            if (lsRetractPos) slideReverse.setPower(0.25);
+            
+            //sets power of clawMotor
+            clawMotor.setPower(-gamepad2.right_trigger); //Close claw
+            clawMotor.setPower(gamepad2.left_trigger); //Open claw
+
+            // clawMotor hold
+            if (gamepad2.right_bumper){
+                clawHold = true;
+            }
+            if (gamepad2.left_bumper){
+                clawHold = false;
+            }
+            if (clawHold){
+                // hold claw closed
+                clawMotor.setPower(-0.4);
+            }
+
+            //controls the height of the linear slide
+            armHeight.setPower(-gamepad2.left_stick_y);//sets the motor controlling arm height equal to the left stick
+
+            //gives drivers control of the servoStick in case it does not pull up
+            if (gamepad2.a) {
+                servoStickRight1.setPosition(1);
+                telemetry.addData("a", "true");
+                telemetry.update();
+            }
+            if (gamepad2.b) {
+                servoStickRight1.setPosition(0);
+                telemetry.addData("b", "true");
+                telemetry.update();
+            }
+            if (gamepad2.x) {
+                servoStickLeft2.setPosition(1);
+                telemetry.addData("Correct", "true");
+                telemetry.update();
+            }
+            if (gamepad2.y) {
+                servoStickLeft2.setPosition(0);
+                telemetry.addData("Wrong", "true");
+                telemetry.update();
+            }
+
+            idle();//waits to be caught up
+
         }
     }
-
     private void getJoyVals()
     {
         y = gamepad1.left_stick_y;
