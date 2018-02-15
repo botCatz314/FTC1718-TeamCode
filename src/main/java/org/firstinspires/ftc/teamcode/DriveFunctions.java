4a8f7aefbce5f0574859b5b43b45ddda293747ba
botCatz314 <botCatz314@gmail.com>
2018-02-14 19:11:02 -0700

    Valentines Day commit

---
+++ src/main/java/org/firstinspires/ftc/teamcode/DriveFunctions.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by ITSA-GAMINGHP2 on 2/7/2018.
+ */
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+public class DriveFunctions {
+    static double colorThreshold =20;
+
+
+
+
+    static public Integer ReadColor(ColorSensor colorSensor){
+        if(colorSensor.red() - colorSensor.blue() > colorThreshold ){
+            return 0;
+        }
+        else if(colorSensor.blue()-colorSensor.red() > colorThreshold){
+            return 1;
+        }
+       else{
+             IllegalArgumentException INVALIDCOLOR;
+             return 2;
+        }
+    }
+
+    static void DriveStraight(DcMotor left, DcMotor right, double speed){
+        right.setPower(speed);
+        left.setPower(speed);
+    }
+    static void BackUp(DcMotor left, DcMotor right, double speed){
+        right.setPower(-speed);
+        left.setPower(-speed);
+    }
+    static void Brake(DcMotor left, DcMotor right){
+        double stop = 0;
+        left.setPower(stop);
+        right.setPower(stop);
+    }
+static public void Turn(double leftsSpeed, double rightSpeed, DcMotor leftMotor, DcMotor rightMotor){
+        leftMotor.setPower(leftsSpeed);
+        rightMotor.setPower(rightSpeed);
+}
+}
---
+++ src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -0,0 +1,200 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+
+/**
+ * Created by ITSA-GAMINGHP2 on 11/9/2017.
+ */
+
+@Autonomous(name = "RedSide", group = "Pushbot" )
+public class TestAuto extends LinearOpMode {
+    private DcMotor leftMotor, rightMotor; //Declares the motors
+    private Servo servoStickLeft2, servoStickRight1, blockFlicker;
+    private ColorSensor colorSensorRight, colorSensorLeft;
+    double powerOff = 0; //creates a variable equal to zero so that the motors can turn off without the use of a magic number
+    BNO055IMU imu; //declares integrated gyro
+    Orientation lastAngle = new Orientation();
+    double threshold = .25, colorThreshold;
+    double right = 1, left = 0;
+    public double pi = 3.1415926535897932;
+
+    @Override
+    public void runOpMode() {
+
+        leftMotor = hardwareMap.dcMotor.get("leftMotor"); //gets properties of left motor from phone
+        rightMotor = hardwareMap.dcMotor.get("rightMotor"); //gets properties for second left motor from phone
+        imu = hardwareMap.get(BNO055IMU.class, "imu"); //gets properties of gyro from phone
+        servoStickLeft2 = hardwareMap.servo.get("servoStickLeft2");
+        servoStickRight1 = hardwareMap.servo.get("servoStickRight1");
+        colorSensorLeft = hardwareMap.colorSensor.get("colorSensorLeft");
+        colorSensorRight = hardwareMap.colorSensor.get("colorSensorRight");
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);//sets the right motors reverse
+        blockFlicker = hardwareMap.servo.get("blockFlicker");
+        SensorManager sensorServices = (SensorManager) hardwareMap.appContext.getSystemService(Context.SENSOR_SERVICE);
+        if (sensorServices != null) {
+            Sensor gyro = sensorServices.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        }
+        //sets parameters of gyro
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled = false;
+        imu.initialize(parameters);
+
+        imu.readCalibrationData(); //calibrates gyro
+        imu.isGyroCalibrated(); //checks that gyro is calibrated
+        //shows user that gyro is calibrated
+
+        /*telemetry.addData("gyro", imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYZ, AngleUnit.DEGREES).firstAngle);
+        telemetry.update();*/
+
+        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        waitForStart(); //waits until the user presses play
+        while (opModeIsActive()) {
+
+        }
+    }
+
+    public void DriveWithEncoders(double distance, double speed) {
+        double off = 0;
+        double encoderCounts = 1120;
+        double driveGearReduction = 4.0;
+        double wheelDiameter = 9;
+        double countsPerMM = (encoderCounts * driveGearReduction) / (wheelDiameter * pi);
+
+        int newLeftTarget;
+        int newRightTarget;
+
+        if (opModeIsActive()) {
+            newLeftTarget = leftMotor.getCurrentPosition() + (int) (countsPerMM * distance);
+            newRightTarget = rightMotor.getCurrentPosition() + (int) (countsPerMM * distance);
+
+            leftMotor.setTargetPosition(newLeftTarget);
+            rightMotor.setTargetPosition(newRightTarget);
+
+            leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            leftMotor.setPower(speed);
+            rightMotor.setPower(speed);
+
+            while (opModeIsActive() && leftMotor.isBusy() && rightMotor.isBusy()) {
+                telemetry.addData("target left position: ", newLeftTarget);
+                telemetry.addData("target right position: ", newRightTarget);
+                telemetry.addData("current left position", leftMotor.getCurrentPosition());
+                telemetry.addData("current right position: ", rightMotor.getCurrentPosition());
+                telemetry.addData("rightMotor: ", rightMotor.isBusy());
+                telemetry.addData("leftMotor", leftMotor.isBusy());
+                telemetry.addData("rightSpeed", rightMotor.getPower());
+                telemetry.addData("leftSpeed", leftMotor.getPower());
+                telemetry.update();
+            }
+            leftMotor.setPower(off);
+            rightMotor.setPower(off);
+
+
+        }
+    }
+
+    public double CalculateError(double desiredAngle) {
+        double error;
+        error = desiredAngle - imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYZ, AngleUnit.DEGREES).firstAngle;
+        telemetry.addData("error", error);
+        telemetry.update();
+        return error;
+    }
+
+    private void turnGyro(double angle, double speed) {
+        telemetry.addData("turn Gyro", angle);
+        telemetry.update();
+        while (opModeIsActive() && !OnHeading(speed, angle)) {
+            telemetry.addData("turning", "check");
+            telemetry.update();
+        }
+    }
+
+    public boolean OnHeading(double speed, double angle) {
+        double error, steer, leftSpeed, rightSpeed;
+        boolean onTarget = false;
+        error = CalculateError(angle);
+        if (error <= threshold) {
+            steer = 0.0;
+            leftSpeed = 0.0;
+            rightSpeed = 0.0;
+            onTarget = true;
+        } else {
+            steer = adjustHeading(error);
+            rightSpeed = -(speed * steer);
+            leftSpeed = -rightSpeed;
+
+        }
+        rightMotor.setPower(rightSpeed);
+        leftMotor.setPower(leftSpeed);
+        return onTarget;
+    }
+
+    private double adjustHeading(double error) {
+        double Kp = .15, Ki = 0, Kd = 0;
+        double errorPrior = 0;
+        double integral = 0, derivative = 0;
+        ElapsedTime turning = new ElapsedTime();
+        turning.reset();
+        while (true) {
+            integral = integral + (error * turning.time());
+            derivative = (error - errorPrior) / turning.time();
+            errorPrior = error;
+            // sleep(1);
+            return Range.clip((error * Kp) + (Ki * integral) + (Kd * derivative), -1, 1);
+
+        }
+    }
+    private void TurnPhoneGyro(){
+        SensorManager sensorServices = (SensorManager) hardwareMap.appContext.getSystemService(Context.SENSOR_SERVICE);
+        if (sensorServices != null) {
+            Sensor gyro = sensorServices.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+            if(gyro != null){
+
+            }
+        }
+
+    }
+    private void TurnGyro2(double Turnamount, double speed){
+       while(Turnamount - imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYZ, AngleUnit.DEGREES).firstAngle >1 )
+        if(Turnamount - imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYZ, AngleUnit.DEGREES).firstAngle < 1
+                && Turnamount - imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYZ, AngleUnit.DEGREES).firstAngle > 1){
+            rightMotor.setPower(powerOff);
+            leftMotor.setPower(powerOff);
+        }
+        else if(Turnamount - imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYZ, AngleUnit.DEGREES).firstAngle > 1){
+            rightMotor.setPower(speed);
+            leftMotor.setPower(-speed);
+        }
+        else if(Turnamount - imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYZ, AngleUnit.DEGREES).firstAngle < 0){
+            leftMotor.setPower(speed);
+            rightMotor.setPower(-speed);
+        }
+        sleep(100);
+    }
+}
